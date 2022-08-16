5e6d4d871563263107040ac1427edc55a23da586
Yash Sankhla <70536159+thesilentcoderr@users.noreply.github.com>
2022-08-16 12:06:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ if st.button("Predict Expression"):
   result=import_and_predict(image)
   st.success('Model has predicted the image expression is   {}'.format(result))
 if st.button("About"):
-  st.header(" Sudhanshu Sharma")
+  st.header("Yash Sankhla")
   st.subheader("Student, Department of Computer Engineering")
   
 html_temp = """
